fd8620b269bf10be4084b0961e5aeb00b6acf197
Szymon <simeno10@gmail.com>
2021-02-13 20:54:05 +0100

    siatka

--- MES.cpp
+++ MES.cpp
@@ -11,7 +11,8 @@ struct element {
     int ID[4];
 };
 struct Globaldata{
-    int H, W, nH, nW, nE, nN;
+    int H=0, W=0, nH=0, nW=0, nN=0;
+	const int nE=15;
 };
 int main()
 {
@@ -19,16 +20,34 @@ int main()
 	ifstream odczyt("dane.txt");
 
     odczyt >> x.H>>x.W>>x.nH>>x.nW;
+	//cout << x.nE << endl;
 
-	element pierwszy;
-	pierwszy.ID[0] = 1;
-	pierwszy.ID[1] = pierwszy.ID[0]+x.nH;
-	pierwszy.ID[2] = pierwszy.ID[1]+1;
-	pierwszy.ID[3] = pierwszy.ID[0]+1;
+	const int a = 15;
+
+	element Elem[a];
+	//int licznik=0;
+	
+	for (int i = 0; i < a; i++) {
+		if(i==0)
+		{
+			Elem[i].ID[0] = 1;
+		}
+		else if(i%5==0)
+		{
+			Elem[i].ID[0] = Elem[i - 1].ID[0] + 2;
+		}
+		else
+		{
+			Elem[i].ID[0] = Elem[i-1].ID[0]+1;
+		}
+		Elem[i].ID[1] = Elem[i].ID[0] + x.nH;
+		Elem[i].ID[2] = Elem[i].ID[1] + 1;
+		Elem[i].ID[3] = Elem[i].ID[0] + 1;
+	}
 
 	odczyt.close(); 
 
-	cout << pierwszy.ID[2];
+	cout << Elem[6].ID[2];
 
 
 	return 0;
