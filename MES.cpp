cc4b032f2d43a6b8270d0c932043e91c36c95d48
Szymon <simeno10@gmail.com>
2021-02-13 19:15:27 +0100

    first commit

---
+++ MES.cpp
@@ -0,0 +1,34 @@
+﻿// MES.cpp : Ten plik zawiera funkcję „main”. W nim rozpoczyna się i kończy wykonywanie programu.
+//
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+struct node {
+    double x, y;
+};
+struct element {
+    int ID[4];
+};
+struct Globaldata{
+    int H, W, nH, nW, nE, nN;
+};
+int main()
+{
+    //int a;
+	Globaldata x;
+	ifstream odczyt("dane.txt");
+
+	odczyt >> x.H;
+
+	//odczyt.close(); 
+
+	//node d{5,7};
+
+	cout << x.H;
+
+
+	return 0;
+}
+
+
